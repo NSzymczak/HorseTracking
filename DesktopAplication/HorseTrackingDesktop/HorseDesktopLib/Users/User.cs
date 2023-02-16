a1ea94301b38e7091a54bc5f6ec9476d6573abc8
Natalia <NSzymczak>
2023-02-16 20:27:42 +0100

    Created LoginView and some needed classes

---
+++ DesktopAplication/HorseTrackingDesktop/HorseDesktopLib/Users/User.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualBasic;
+
+namespace HorseDesktopLib.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Hash { get; set; }
+        public string Salt { get; set; }
+
+        public PeopleDetails Details { get; set; }
+        public UserType Type { get; set; }
+
+        public DateAndTime CreatedDateTime { get; set; }
+
+
+        public static User CurrentUser { get; set; }
+    }
+}
---
+++ DesktopAplication/HorseTrackingDesktop/HorseTrackingDesktop/Database/HorseTrackingContext.cs
@@ -0,0 +1,30 @@
+﻿using HorseDesktopLib.Horses;
+using HorseDesktopLib.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace HorseTrackingDesktop.Database
+{
+    public class HorseTrackingContext: DbContext
+    {
+        public string ConnectionString { get; }
+
+        public HorseTrackingContext(string connectionString)
+        {
+            this.ConnectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(this.ConnectionString);
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Horse> Horse { get; set; }
+        public DbSet<HorseStatus> HorseStatuses { get; set;}
+        public DbSet<HorseGender> HorseGender { get; set;}
+
+       
+
+    }
+}
