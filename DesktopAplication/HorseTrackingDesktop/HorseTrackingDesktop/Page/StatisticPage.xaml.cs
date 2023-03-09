aaae2466dd83c163fa22ee3ab6b3f61caeee8826
Natalia <NSzymczak>
2023-03-09 14:04:09 +0100

    rename page from View to Page

--- DesktopAplication/HorseTrackingDesktop/HorseTrackingDesktop/Page/StatisticPage.xaml.cs
+++ DesktopAplication/HorseTrackingDesktop/HorseTrackingDesktop/Page/StatisticPage.xaml.cs
@@ -6,13 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using HorseTrackingDesktop.PageModel;
 
 namespace HorseTrackingDesktop.View
 {
@@ -21,11 +15,11 @@ namespace HorseTrackingDesktop.View
     /// </summary>
     public partial class StatisticPage : Page
     {
-        StatisticViewModel viewModel;
+        StatisticPageModel viewModel;
         public StatisticPage()
         {
             InitializeComponent();
-            viewModel = new StatisticViewModel();
+            viewModel = new StatisticPageModel();
             DataContext = viewModel;
         }
 
